2b59dd475fe2b5fb3b89a7da1d49dff988c5590c
XanaDevelops <xana@DESKTOP-84LC1BU.localdomain>
2020-05-04 00:31:17 +0200

    Añadido cabecera de licencia a los programas

--- Data/FrontEnd.py
+++ Data/FrontEnd.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
+'''
+FrontEnd de ModManager 2.x
+    Copyright (C) 2020  Daniel García Vázquez
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 ################
 ### ManagerMods para MC todas versiones v2.0.0 beta
 ### codigo por Daniel G.
--- Data/data/Custom.py
+++ Data/data/Custom.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+'''
+Script para mostrar fuentes externas de ModManager 2.x
+    Copyright (C) 2020  Daniel García Vázquez
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+# Modificado: Daniel García Vázquez
+## bajo licencia GNU GPL v3
 # Author: Miguel Martinez Lopez
 ## bajo la licencia MIT
 
